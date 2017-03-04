1716c6793568224066042777a0ef98a9ee3de83a
[KBoohene] <okwabse@gmail.com>
2017-03-04 20:32:43 +0000

    created funcion headers

--- Classes/order.php
+++ Classes/order.php
@@ -1,2 +1,46 @@
 <?php
+
+include_once('../adb.php');
+class order{
+  function order(){
+
+  }
+
+  function addToCart($itemId){
+
+  }
+
+  function removeFromCart($itemId){
+
+  }
+
+  function getCustomerOrders($customerId){
+
+  }
+
+  function getOrderDetails($orderId){
+
+  }
+
+  function emptyCart(){
+
+  }
+
+  function checkout(){
+
+  }
+
+  function getDates($orderId){
+
+  }
+
+  function getNumOrdersPerWeek(){
+
+  }
+
+  function updateDates($orderId, $shippingDate, $deliveryDate){
+
+  }
+}
+
 ?>
