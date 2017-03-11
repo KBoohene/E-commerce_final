e4715c4857598bcdff8801ac920e26728770be72
[KBoohene] <okwabse@gmail.com>
2017-03-11 15:21:54 +0000

    retested smarty template

---
+++ tmp/55b25caa577edab11d69dbd0a737dda40e7d6618_0.file.searchItems.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-03-11 16:16:37
+  from "C:\xampp\htdocs\E-commerce_final\views\searchItems.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_58c414d5a51cc4_66623756',
+  'has_nocache_code' => false,
+  'file_dependency' =>
+  array (
+    '55b25caa577edab11d69dbd0a737dda40e7d6618' =>
+    array (
+      0 => 'C:\\xampp\\htdocs\\E-commerce_final\\views\\searchItems.tpl',
+      1 => 1489241504,
+      2 => 'file',
+    ),
+  ),
+  'includes' =>
+  array (
+  ),
+),false)) {
+function content_58c414d5a51cc4_66623756 (Smarty_Internal_Template $_smarty_tpl) {
+}
+}
