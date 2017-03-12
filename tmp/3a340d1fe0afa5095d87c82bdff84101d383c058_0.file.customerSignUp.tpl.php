ac5e9073b289c9d027f5a5ff96c6baeec988afc8
[KBoohene] <okwabse@gmail.com>
2017-03-12 20:35:58 +0000

    Added login and sign up links

---
+++ tmp/3a340d1fe0afa5095d87c82bdff84101d383c058_0.file.customerSignUp.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-03-12 21:27:30
+  from "C:\xampp\htdocs\E-commerce_final\views\customerSignUp.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_58c5af32870e59_67535870',
+  'has_nocache_code' => false,
+  'file_dependency' =>
+  array (
+    '3a340d1fe0afa5095d87c82bdff84101d383c058' =>
+    array (
+      0 => 'C:\\xampp\\htdocs\\E-commerce_final\\views\\customerSignUp.tpl',
+      1 => 1489336459,
+      2 => 'file',
+    ),
+  ),
+  'includes' =>
+  array (
+  ),
+),false)) {
+function content_58c5af32870e59_67535870 (Smarty_Internal_Template $_smarty_tpl) {
+}
+}
--- tmp/4ebd2c4f7586f94da526d626f99b50b33a11fffa_0.file.landingPage.tpl.php
+++ tmp/4ebd2c4f7586f94da526d626f99b50b33a11fffa_0.file.landingPage.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-03-12 19:51:00
+/* Smarty version 3.1.30, created on 2017-03-12 21:34:05
   from "C:\xampp\htdocs\E-commerce_final\views\landingPage.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58c59894538502_44756766',
+  'unifunc' => 'content_58c5b0bd413862_34419586',
   'has_nocache_code' => false,
   'file_dependency' =>
   array (
     '4ebd2c4f7586f94da526d626f99b50b33a11fffa' =>
     array (
       0 => 'C:\\xampp\\htdocs\\E-commerce_final\\views\\landingPage.tpl',
-      1 => 1489336459,
+      1 => 1489350788,
       2 => 'file',
     ),
   ),
@@ -20,12 +20,27 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_58c59894538502_44756766 (Smarty_Internal_Template $_smarty_tpl) {
+function content_58c5b0bd413862_34419586 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <html>
-  <head></head>
+  <head>
+    <title>Home Page</title>
+  </head>
   <body>
-    <p>Test</p>
+    <a href="index.php?cAction=4">Login</a>
+    <a href="index.php?cAction=3">Sign Up</a>
+
+    <div>
+      <form action="index.php?cAction=1" method="POST">
+      <div>
+        <input class="search-bar" id="search" type="text" name="searchName">
+      </div>
+      <div>
+        <button type="submit" class="button">Search</button>
+      </div>
+      </form>
+    </div>
+
   </body>
 </html>
 <?php }
