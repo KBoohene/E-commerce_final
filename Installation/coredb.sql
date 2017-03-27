74e9fcb9c69cae85f4219b53f89ba177b8930233
ydasilva <youssoufdasilva@gmail.com>
2017-03-27 19:50:47 +0000

    Revert "Youssouf version2"

--- Installation/coredb.sql
+++ Installation/coredb.sql
@@ -1,15 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.7
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2017 at 10:18 PM
--- Server version: 5.6.31
--- PHP Version: 5.6.25
+-- Generation Time: Feb 28, 2017 at 06:47 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.5.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
+USE coredb;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,13 +23,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `account_type`
+--
+
+CREATE TABLE `account_type` (
+  `ID` int(4) NOT NULL,
+  `Type` char(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `account_type`
+--
+
+INSERT INTO `account_type` (`ID`, `Type`) VALUES
+(1, 'Customer'),
+(2, 'Employee'),
+(3, 'Administrator');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
+CREATE TABLE `categories` (
   `catno` int(11) NOT NULL,
   `catname` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `categories`
@@ -49,7 +69,7 @@ INSERT INTO `categories` (`catno`, `catname`) VALUES
 -- Table structure for table `checkout_log`
 --
 
-CREATE TABLE IF NOT EXISTS `checkout_log` (
+CREATE TABLE `checkout_log` (
   `ID` int(11) NOT NULL,
   `order_no` bigint(5) NOT NULL,
   `person_id` int(11) NOT NULL,
@@ -63,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `checkout_log` (
 -- Table structure for table `customers`
 --
 
-CREATE TABLE IF NOT EXISTS `customers` (
+CREATE TABLE `customers` (
   `cno` int(11) NOT NULL,
   `cname` varchar(30) NOT NULL,
   `street` varchar(30) NOT NULL,
@@ -71,19 +91,15 @@ CREATE TABLE IF NOT EXISTS `customers` (
   `phone` char(12) NOT NULL,
   `Username` varchar(20) NOT NULL,
   `Password` varchar(50) NOT NULL,
-  `status` varchar(15) NOT NULL DEFAULT 'enabled',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`cno`, `cname`, `street`, `zip`, `phone`, `Username`, `Password`, `status`, `created_at`) VALUES
-(10, 'Kwabena', 'hnoome', 66242018, '0265057763', 'kwabena.boohene', 'jumper', 'enabled', '2017-03-19 02:05:41'),
-(11, 'Kofi Boamah', 'House', 52522017, '0265057762', 'kofi.boamah', 'Tsuchikage14', 'enabled', '2017-03-19 02:05:21'),
-(12, 'Yaw Koom', '5679099', 52362019, '026509987', 'yaw.koom', 'yawkoom', 'enabled', '2017-03-16 20:51:43'),
-(13, 'Youssouf da Silva', 'myStreet', 52732017, '123456789', 'youssouf', 'youssouf', 'enabled', '2017-03-26 20:36:34');
+INSERT INTO `customers` (`cno`, `cname`, `street`, `zip`, `phone`, `Username`, `Password`, `created_at`) VALUES
+(10, 'Kwabena', 'hnoome', 52522017, '0265057796', 'kwabena.boohene', 'jumper', '2017-02-28 13:58:08');
 
 -- --------------------------------------------------------
 
@@ -91,25 +107,16 @@ INSERT INTO `customers` (`cno`, `cname`, `street`, `zip`, `phone`, `Username`, `
 -- Table structure for table `employees`
 --
 
-CREATE TABLE IF NOT EXISTS `employees` (
+CREATE TABLE `employees` (
   `eno` int(11) NOT NULL,
   `ename` varchar(30) NOT NULL,
   `zip` bigint(5) NOT NULL,
   `hdate` date NOT NULL,
   `Password` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `account_type` set('2','3') NOT NULL DEFAULT '2',
+  `account_type` int(11) NOT NULL,
   `Username` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `employees`
---
-
-INSERT INTO `employees` (`eno`, `ename`, `zip`, `hdate`, `Password`, `created_at`, `account_type`, `Username`) VALUES
-(4, 'David Okyere', 52362019, '2017-03-08', 'david', '2017-03-16 12:00:06', '2', 'david.okyere'),
-(5, 'Kwabena Boohene', 47852018, '0000-00-00', 'kwabena', '2017-03-16 12:00:51', '2', 'k.boohene'),
-(6, 'youssouf silva', 52732017, '2017-03-03', 'youssouf', '2017-03-16 12:01:24', '3', 'k.boohene');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -117,34 +124,33 @@ INSERT INTO `employees` (`eno`, `ename`, `zip`, `hdate`, `Password`, `created_at
 -- Table structure for table `items`
 --
 
-CREATE TABLE IF NOT EXISTS `items` (
+CREATE TABLE `items` (
   `ino` bigint(5) NOT NULL,
   `iname` varchar(30) NOT NULL,
-  `idesc` text NOT NULL,
   `qoh` int(11) NOT NULL,
   `price` decimal(6,2) NOT NULL,
   `olevel` int(11) NOT NULL,
   `catno` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`ino`, `iname`, `idesc`, `qoh`, `price`, `olevel`, `catno`) VALUES
-(1, 'Nike Flanel pants', 'This is a description of Nike Flanel pants', 50, 50.00, 10, 1),
-(2, 'Spalding Long Racket', 'This is a description of Spalding Long Racket', 50, 25.00, 10, 5),
-(3, 'Addidas NMD', 'This is a description of Addidas NMD', 20, 230.00, 5, 3),
-(4, 'Nike Air Jordans', 'This is a description of Nike Air Jordans', 30, 200.00, 5, 3),
-(5, 'Spalding 360 Gloves', 'This is a description of Spalding 360 Gloves', 40, 25.00, 10, 6),
-(6, 'Gildan Plain White Tees', 'This is a description of Gildan Plain White Tees', 30, 40.00, 10, 2),
-(7, 'Addidas slim fit Joggers', 'This is a description of Addidas slim fit Joggers', 50, 20.00, 10, 4),
-(8, 'Spalding grey sweats', 'This is a description of Spalding grey sweats', 40, 15.00, 12, 4),
-(9, 'Nike Roshes Blue', 'This is a description of Nike Roshes Blue', 60, 20.00, 10, 3),
-(10, 'Adidas Barricade', 'This is a description of Adidas Barricade', 40, 250.00, 10, 5),
-(11, 'Nike Roshes v2', 'This is a description of Nike Roshes v2', 50, 20.00, 20, 1),
-(18, 'Adidas NMDs V2', 'This is a description of Adidas NMDs V2', 50, 20.00, 10, 3),
-(19, 'Plim Soles', 'This is a description of Plim Soles', 40, 20.00, 10, 3);
+INSERT INTO `items` (`ino`, `iname`, `qoh`, `price`, `olevel`, `catno`) VALUES
+(1, 'Nike Flanel pants', 50, '50.00', 10, 1),
+(2, 'Spalding Long Racket', 50, '25.00', 10, 5),
+(3, 'Addidas NMD', 20, '230.00', 5, 3),
+(4, 'Nike Air Jordans', 30, '200.00', 5, 3),
+(5, 'Spalding 360 Gloves', 40, '25.00', 10, 6),
+(6, 'Gildan Plain White Tees', 30, '40.00', 10, 2),
+(7, 'Addidas slim fit Joggers', 50, '20.00', 10, 4),
+(8, 'Spalding grey sweats', 40, '15.00', 12, 4),
+(9, 'Nike Roshes Blue', 60, '20.00', 10, 3),
+(10, 'Adidas Barricade', 40, '250.00', 10, 5),
+(11, 'Nike Roshes v2', 50, '20.00', 20, 1),
+(18, 'Adidas NMDs V2', 50, '20.00', 10, 3),
+(19, 'Plim Soles', 40, '20.00', 10, 3);
 
 -- --------------------------------------------------------
 
@@ -152,12 +158,12 @@ INSERT INTO `items` (`ino`, `iname`, `idesc`, `qoh`, `price`, `olevel`, `catno`)
 -- Table structure for table `login_log`
 --
 
-CREATE TABLE IF NOT EXISTS `login_log` (
+CREATE TABLE `login_log` (
   `ID` int(11) NOT NULL,
   `PersonID` int(11) NOT NULL,
   `LogInTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `account_type` set('1','2','3') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `account_type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -165,18 +171,11 @@ CREATE TABLE IF NOT EXISTS `login_log` (
 -- Table structure for table `odetails`
 --
 
-CREATE TABLE IF NOT EXISTS `odetails` (
+CREATE TABLE `odetails` (
   `ono` bigint(5) NOT NULL,
   `ino` bigint(5) NOT NULL,
   `qty` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `odetails`
---
-
-INSERT INTO `odetails` (`ono`, `ino`, `qty`) VALUES
-(3, 18, 1);
 
 -- --------------------------------------------------------
 
@@ -184,34 +183,12 @@ INSERT INTO `odetails` (`ono`, `ino`, `qty`) VALUES
 -- Table structure for table `orders`
 --
 
-CREATE TABLE IF NOT EXISTS `orders` (
+CREATE TABLE `orders` (
   `ono` bigint(5) NOT NULL,
   `cno` int(11) NOT NULL,
-  `checked_out` varchar(3) NOT NULL,
   `received` date NOT NULL,
   `shipped` date NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`ono`, `cno`, `checked_out`, `received`, `shipped`, `created_at`) VALUES
-(1, 10, 'No', '2017-03-05', '0000-00-00', '2017-03-19 02:31:36'),
-(2, 11, 'No', '2017-03-01', '0000-00-00', '2017-03-19 02:22:08'),
-(3, 13, 'No', '0000-00-00', '0000-00-00', '2017-03-26 20:36:58');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `visitors_log`
---
-
-CREATE TABLE IF NOT EXISTS `visitors_log` (
-  `IP_address` varchar(32) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -220,7 +197,7 @@ CREATE TABLE IF NOT EXISTS `visitors_log` (
 -- Table structure for table `zipcodes`
 --
 
-CREATE TABLE IF NOT EXISTS `zipcodes` (
+CREATE TABLE `zipcodes` (
   `zip` bigint(5) NOT NULL,
   `city` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -243,6 +220,12 @@ INSERT INTO `zipcodes` (`zip`, `city`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `account_type`
+--
+ALTER TABLE `account_type`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `categories`
@@ -270,6 +253,7 @@ ALTER TABLE `customers`
 --
 ALTER TABLE `employees`
   ADD PRIMARY KEY (`eno`),
+  ADD UNIQUE KEY `account_type` (`account_type`),
   ADD KEY `zip` (`zip`);
 
 --
@@ -284,7 +268,8 @@ ALTER TABLE `items`
 --
 ALTER TABLE `login_log`
   ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `PersonID` (`PersonID`);
+  ADD UNIQUE KEY `PersonID` (`PersonID`),
+  ADD UNIQUE KEY `account_type` (`account_type`);
 
 --
 -- Indexes for table `odetails`
@@ -301,12 +286,6 @@ ALTER TABLE `orders`
   ADD KEY `cno` (`cno`);
 
 --
--- Indexes for table `visitors_log`
---
-ALTER TABLE `visitors_log`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `zipcodes`
 --
 ALTER TABLE `zipcodes`
@@ -320,7 +299,7 @@ ALTER TABLE `zipcodes`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `catno` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `catno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `checkout_log`
 --
@@ -330,32 +309,27 @@ ALTER TABLE `checkout_log`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `cno` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `cno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `eno` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `eno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `ino` bigint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+  MODIFY `ino` bigint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `login_log`
 --
 ALTER TABLE `login_log`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `ono` bigint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `visitors_log`
---
-ALTER TABLE `visitors_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ono` bigint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -378,14 +352,16 @@ ALTER TABLE `customers`
 -- Constraints for table `employees`
 --
 ALTER TABLE `employees`
-  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`zip`) REFERENCES `zipcodes` (`zip`);
+  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`zip`) REFERENCES `zipcodes` (`zip`),
+  ADD CONSTRAINT `employees_ibfk_2` FOREIGN KEY (`account_type`) REFERENCES `account_type` (`ID`);
 
 --
 -- Constraints for table `login_log`
 --
 ALTER TABLE `login_log`
   ADD CONSTRAINT `login_log_ibfk_1` FOREIGN KEY (`PersonID`) REFERENCES `customers` (`cno`),
-  ADD CONSTRAINT `login_log_ibfk_2` FOREIGN KEY (`PersonID`) REFERENCES `employees` (`eno`);
+  ADD CONSTRAINT `login_log_ibfk_2` FOREIGN KEY (`PersonID`) REFERENCES `employees` (`eno`),
+  ADD CONSTRAINT `login_log_ibfk_3` FOREIGN KEY (`account_type`) REFERENCES `account_type` (`ID`);
 
 --
 -- Constraints for table `odetails`
